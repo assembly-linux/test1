e33dcd6646f701a99fd0e6ab294210a52cc3673c
Dionis Papavramidis <dionis.papavramidis@gmail.com>
2019-09-30 23:05:04 -0700

    switching to python3

--- pack2/ex12.py
+++ pack2/ex12.py
@@ -1,9 +1,5 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 #encoding: UTF-8
 
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
 if __name__ == "__main__":
-    print "Hello World"
+    print("Hello World")
